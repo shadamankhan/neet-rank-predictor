5fde0cd720eaad883d4d7cedd0b7af66eb39ffe1
shadamankhan <shad_aman@yahoo.co.in>
2026-01-17 12:40:34 +0530

    fix: add missing useEffect import in TestValidator

--- frontend/src/pages/admin/TestValidator.jsx
+++ frontend/src/pages/admin/TestValidator.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LatexRenderer from '../../components/LatexRenderer';
 import { preprocessContent, groupQuestionsIntoSections } from '../../utils/examUtils';
 import { getApiBase } from '../../apiConfig';
